bcd81a0fb2926428d90d74e040eaa6f8b94a57da
Amangeldi Assylbek <amangeldyasylbek452@gmail.com>
2026-02-14 13:12:36 +0500

    pp

---
+++ practice 2/Python Operators.py
@@ -0,0 +1,56 @@
+print(10 + 5)
+
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+
+
+numbers = [1, 2, 3, 4, 5]
+
+if (count := len(numbers)) > 3:
+    print(f"List has {count} elements")
+
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+x = 5
+
+print(not(x > 3 and x < 10))
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+
+
+fruits = ["apple", "banana", "cherry"]
+
+print("banana" in fruits)
+
+
+print(6 | 3)
+
+
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
