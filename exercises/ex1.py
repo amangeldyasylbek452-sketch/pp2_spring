2340023a91b30cdec463fc8cbd26b4a47e90a9e8
Amangeldi Assylbek <amangeldyasylbek452@gmail.com>
2026-02-28 13:29:30 +0500

    .

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -1,5 +1,28 @@
+# Convert degrees to radians
 import math
 
-a = int(input("Enter a number: "))
+a = int(input())
 b = math.radians(a)
 print(b)
+
+
+#area of a trapezoid
+a = int(input())
+b = int(input())
+h = int(input())
+S = (a + b) * h / 2
+print(S)
+
+
+#area of regular polygon
+n = int(input())
+s = int(input())
+S = n * s**2 / (4 * math.tan(math.pi / n))
+print(S)
+
+
+#area of a parallelogram
+b = int(input())
+h = int(input())
+S = b * h
+print(S)
